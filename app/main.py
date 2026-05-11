a553ebcd81de664dab1cbbd0539b24c85c55089d
Pandidharan22 <pandidharan7@gmail.com>
2026-05-11 19:19:16 +0530

    FastAPI foundation and server initialized

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="Self-Reflective RAG API",
+    description = "Backend for heuristic based self-healing RAG system.",
+    version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/health")
+async def health_check():
+    return {"status": "Online",
+            "message": "API is ready"
+            }
